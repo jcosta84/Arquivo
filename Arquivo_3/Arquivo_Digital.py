ade746dd22cf7d8f52adc14a689490c33042d0c2
jcosta84 <cpjcosta30@gmail.com>
2026-09-24 10:14:39 -0100

    feito

--- Arquivo_3/Arquivo_Digital.py
+++ Arquivo_3/Arquivo_Digital.py
@@ -16,7 +16,7 @@ from db import get_connection, fetch_all, fetch_one, execute
 
 
 # =========================================================
-# CONFIGURAÇÃO
+# CONFIGURAÇÃO0
 # =========================================================
 
 ctk.set_appearance_mode("Dark")
